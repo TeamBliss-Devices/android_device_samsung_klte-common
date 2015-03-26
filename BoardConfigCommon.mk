619fa1da8e08cf53c623b8df74634c1001409479
Bryan Clay <lost.junglist@gmail.com>
2015-03-25 17:05:02 -0700

    UBERTC toolchains
    Signed-off-by: Bryan Clay <lost.junglist@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,12 +103,12 @@ WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
 -include vendor/samsung/klte-common/BoardConfigVendor.mk
 
 # BlissPop Config Flags
-TARGET_TC_ROM := 4.8-sm
-TARGET_TC_KERNEL := 4.8-sm
+TARGET_TC_ROM := 4.8-linaro
+TARGET_TC_KERNEL := 4.8-linaro
 BLISSIFY := true
 BLISS_O3 := true
 BLISS_STRICT := true
-BLISS_GRAPHITE := true
+BLISS_GRAPHITE := false
 BLISS_KRAIT := true
 BLISS_PIPE := true
 TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
